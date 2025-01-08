b7580c29f6f631645e0db04b81177a3ff0a2987f
Harry <2411mail@gmail.com>
2025-01-08 17:01:17 +0800

    we do not need to compute hash with encrypted values

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,18 @@ use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32, ClientKey, F
 use tfhe::boolean::prelude::{ClientKey as ClientKeyBool, Ciphertext, gen_keys};
 use crate::sha256_bool::{pad_sha256_input, bools_to_hex, sha256_fhe as sha256_fhe_bool};
 use crate::sha256::{sha256_fhe, encrypt_data, decrypt_hash};
+use crate::schnorr::{Schnorr, hash};
 
 
-fn hash( message: &str) -> u32 {
-    let mut hasher_input = Vec::new();
-    hasher_input.extend(message.as_bytes());
-    let mut hasher = Sha256::new();
-    hasher.update(&hasher_input);
-    let hash_result = hasher.finalize();
-    // todo: only take the first 2 bytes
-    u32::from_be_bytes(hash_result[..4].try_into().expect("Hash output too short")) & 0xFFFF
-}
+// fn hash(message: &str) -> u32 {
+//     let mut hasher_input = Vec::new();
+//     hasher_input.extend(message.as_bytes());
+//     let mut hasher = Sha256::new();
+//     hasher.update(&hasher_input);
+//     let hash_result = hasher.finalize();
+//     // todo: only take the first 2 bytes
+//     u32::from_be_bytes(hash_result[..4].try_into().expect("Hash output too short")) & 0xFFFF
+// }
 
 fn hash_encrypted(encrypted_input: Vec<tfhe::FheUint<tfhe::FheUint32Id>>, client_key: Option<&ClientKey>) -> Result<FheUint32, Box<dyn std::error::Error>> {
     let encrypted_hash = sha256_fhe(encrypted_input);
@@ -54,7 +55,6 @@ struct FheSchnorr {
     g: u32,
     private_key_encrypted: FheUint32,
     public_key_encrypted: FheUint32,
-    g_encrypted: FheUint32,
 }
 
 // implement fhe schnorr protocol, all operations use fhe
@@ -64,55 +64,71 @@ impl FheSchnorr {
         let public_key = private_key * g;
         let private_key_encrypted = FheUint32::try_encrypt(private_key, client_key)?;
         let public_key_encrypted = FheUint32::try_encrypt(public_key, client_key)?;
-        let g_encrypted = FheUint32::try_encrypt(g, client_key)?;
         Ok(Self {
             private_key,
             public_key,
             g,
             private_key_encrypted,
             public_key_encrypted,
-            g_encrypted,
         })
     }
 
-    fn sign(&self, message: &str, client_key: &ClientKey) -> Result<(u32, FheUint32), Box<dyn std::error::Error>> {
+    fn sign(&self, message: &str, client_key: &ClientKey) -> Result<(u32, u64), Box<dyn std::error::Error>> {
         // 1. generate a random number k
         let k = rand::thread_rng().gen_range(0..=255);
+        let k_encrypted = FheUint32::try_encrypt(k, client_key)?;
         // 2. calculate r = k * G
         let r = k * self.g;
         // 3. calculate public key pk = private_key * G
         let pk = self.private_key * self.g;
         // 4. calculate e = hash(r || pk || message)
-        let message_hash = hash(message);
+        // let message_hash = hash(message);
+        let e = hash(r, pk, message);
+        let e_encrypted = FheUint32::try_encrypt(e, client_key)?;
         // let message_hash_encrypted = FheUint32::try_encrypt(message_hash, &self.client_key)?;
 
-        let input = r.to_string() + &pk.to_string() + &message_hash.to_string();
-        let buf = input.as_bytes().to_vec();
-        let encrypted_input = encrypt_data(buf, Some(client_key));
-        let e_encrypted = hash_encrypted(encrypted_input, Some(client_key)).unwrap();
-
+        // let input = r.to_string() + &pk.to_string() + &message_hash.to_string();
+        // let buf = input.as_bytes().to_vec();
+        // let encrypted_input = encrypt_data(buf, Some(client_key));
+        // let e_encrypted = hash_encrypted(encrypted_input, Some(client_key)).unwrap();
+        println!("e_encrypted finished");
         // 5. calculate s = k + e * private_key
-        let s_encrypted = k + e_encrypted * self.private_key_encrypted.clone();
+        let s_encrypted = k_encrypted + e_encrypted * self.private_key_encrypted.clone();
+        println!("s_encrypted finished");
+        let s = s_encrypted.decrypt(client_key);
+        println!("s finished");
         // 6. return signature (r, s)
-        Ok((r, s_encrypted))
+        Ok((r, s))
     }
 
-    fn verify(&self, message: &str, signature: (u32, FheUint32), client_key: &ClientKey) -> Result<FheBool, Box<dyn std::error::Error>> {
-        // verify: s * g = r + e * pk
-        // s_encrypted * g_encrypted = r_encrypted + e_encrypted * pk_encrypted
-        let (r, s_encrypted) = signature;
-        let pk = self.public_key_encrypted.clone();
-        let message_hash = hash(message);
-        let input = r.to_string() + &self.public_key.to_string() + &message_hash.to_string();
-        let buf = input.as_bytes().to_vec();
-        let encrypted_input = encrypt_data(buf, Some(client_key));
-        let e_encrypted = hash_encrypted(encrypted_input, Some(client_key)).unwrap();
+    // verify: s * g ?= r + e * pk
+    // s_encrypted * g ?= r + e_encrypted * pk_encrypted
+    // equals to: s_encrypted * g ?= k * g + e_encrypted * private_key_encrypted * g
+    // we only need to check if s_encrypted == k + private_key_encrypted
+    // fn verify(&self, message: &str, signature: (u32, u32), client_key: &ClientKey) -> Result<FheBool, Box<dyn std::error::Error>> {
+    //     let (r, s) = signature;
+    //     let pk = self.public_key_encrypted.clone();
 
-        let r_encrypted = FheUint32::try_encrypt(r, client_key)?;
+    //     let message_hash = hash(message);
+    //     let input = r.to_string() + &self.public_key.to_string() + &message_hash.to_string();
+    //     let buf = input.as_bytes().to_vec();
+    //     let encrypted_input = encrypt_data(buf, Some(client_key));
+    //     let e_encrypted = hash_encrypted(encrypted_input, Some(client_key)).unwrap();
 
-        let s_g = s_encrypted.clone() * self.g_encrypted.clone();
-        let r_e_pk = r_encrypted.clone() + e_encrypted * pk.clone();
-        Ok(s_g.eq(&r_e_pk))
+    //     let s_g = s * self.g;
+    //     let r_e_pk = r + e_encrypted * self.public_key;
+    //     Ok(s_g == r_e_pk)
+    // }
+    pub fn verify(&self, message: &str, signature: (u32, u64)) -> bool {
+        // 1. get the signature
+        let (r, s) = signature;
+        // 2. get the public key
+        let pk = self.public_key;
+        // 3. calculate e = hash(r || pk || message)
+        let e = hash(r, pk, message);
+        // 4. verify the signature: s * G = r + e * pk
+        assert_eq!(s * self.g as u64, r as u64 + (e as u64) * pk as u64);
+        true
     }
 }
 
@@ -125,6 +141,14 @@ mod tests {
         let total_timer = Instant::now();
         let private_key = 2025010716;
         let message = "Hello World";
+
+        let schnorr = Schnorr::new(private_key);
+        let signature_original = schnorr.sign(message);
+        let result = schnorr.verify(message, signature_original);
+        assert!(result);
+
+        println!("original signature: {:?}", signature_original);
+
         println!("start");
         let config = ConfigBuilder::default().build();
         println!("config");
@@ -132,25 +156,30 @@ mod tests {
         println!("generate_keys");
         set_server_key(server_keys);
         println!("server keys");
+
         let fhe_schnorr = FheSchnorr::new(private_key, &client_key).unwrap();
         println!("fhe schnorr");
 
         let start = Instant::now();
-        let signature = fhe_schnorr.sign(message, &client_key).unwrap();
+        let signature_fhe = fhe_schnorr.sign(message, &client_key).unwrap();
         let end = Instant::now();
+        println!("signature_fhe: {:?}", signature_fhe);
         println!("time taken for sign: {:?}", end.duration_since(start));
+        // do the check
+        assert_eq!(signature_original.0, signature_fhe.0);
+        assert_eq!(signature_original.1, signature_fhe.1);
 
-        let start = Instant::now();
-        let result = fhe_schnorr.verify(message, signature, &client_key).unwrap();
-        let end = Instant::now();
-        println!("time taken for verify: {:?}", end.duration_since(start));
+        // let start = Instant::now();
+        // let result = fhe_schnorr.verify(message, signature_fhe);
+        // let end = Instant::now();
+        // println!("time taken for verify: {:?}", end.duration_since(start));
 
-        let start = Instant::now();
-        let decrypted_result = result.decrypt(&client_key);
-        let end = Instant::now();
-        println!("time taken for decrypt: {:?}", end.duration_since(start));
+        // let start = Instant::now();
+        // let decrypted_result = result.decrypt(&client_key);
+        // let end = Instant::now();
+        // println!("time taken for decrypt: {:?}", end.duration_since(start));
 
-        assert!(decrypted_result);
+        // assert!(decrypted_result);
 
         println!("end");
         println!("Total time: {:?}", total_timer.elapsed());
--- src/schnorr.rs
+++ src/schnorr.rs
@@ -1,32 +1,33 @@
 use sha2::{Sha256, Digest};
 use rand::Rng;
 
-struct Schnorr {
-    private_key: u64,
-    public_key: u64,
-    g: u64,
+
+pub fn hash(r: u32, pk: u32, message: &str) -> u32 {
+    let mut hasher_input = Vec::new();
+    hasher_input.extend(&r.to_be_bytes());
+    hasher_input.extend(&pk.to_be_bytes());
+    hasher_input.extend(message.as_bytes());
+    let mut hasher = Sha256::new();
+    hasher.update(&hasher_input);
+    let hash_result = hasher.finalize();
+    let result_u32 = u32::from_be_bytes(hash_result[..4].try_into().expect("Hash output too short"));
+    result_u32 as u32
+}
+
+pub struct Schnorr {
+    private_key: u32,
+    public_key: u32,
+    g: u32,
 }
 
 impl Schnorr {
-    fn new(private_key: u64) -> Self {
-        let g: u64 = 2; // Define G
+    pub fn new(private_key: u32) -> Self {
+        let g: u32 = 2; // Define G
         let public_key = private_key * g;
         Self { private_key, public_key, g }
     }
 
-    fn hash(&self, r: u64, pk: u64, message: &str) -> u64 {
-        let mut hasher_input = Vec::new();
-        hasher_input.extend(&r.to_be_bytes());
-        hasher_input.extend(&pk.to_be_bytes());
-        hasher_input.extend(message.as_bytes());
-        let mut hasher = Sha256::new();
-        hasher.update(&hasher_input);
-        let hash_result = hasher.finalize();
-        let result_u32 = u32::from_be_bytes(hash_result[..4].try_into().expect("Hash output too short"));
-        result_u32 as u64
-    }
-
-    fn sign(&self, message: &str) -> (u64, u64) {
+    pub fn sign(&self, message: &str) -> (u32, u64) {
         // 1. generate a random number k
         let k = rand::thread_rng().gen_range(0..=255);
         // 2. calculate r = k * G
@@ -34,23 +35,23 @@ impl Schnorr {
         // 3. calculate public key pk = private_key * G
         let pk = self.private_key * self.g;
         // 4. calculate e = hash(r || pk || message)
-        let e = self.hash(r, pk, message);
+        let e = hash(r, pk, message);
         println!("e: {}", e);
         // 5. calculate s = k + e * private_key
-        let s = k + e * self.private_key;
+        let s = k as u64 + (e as u64) * self.private_key as u64;
         // 6. return signature (r, s)
         (r, s)
     }
 
-    fn verify(&self, message: &str, signature: (u64, u64)) -> bool {
+    pub fn verify(&self, message: &str, signature: (u32, u64)) -> bool {
         // 1. get the signature
         let (r, s) = signature;
         // 2. get the public key
         let pk = self.public_key;
         // 3. calculate e = hash(r || pk || message)
-        let e = self.hash(r, pk, message);
+        let e = hash(r, pk, message);
         // 4. verify the signature: s * G = r + e * pk
-        assert_eq!(s * self.g, r + e * pk);
+        assert_eq!(s * self.g as u64, r as u64 + (e as u64) * pk as u64);
         true
     }
 }
